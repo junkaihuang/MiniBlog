4878e67b1cb5df34911adf2159235cbd7d87e8e6
junkaihuang <junkaihuang@qq.com>
2018-05-27 21:24:29 +0800

    添加项目文件。

---
+++ MiniBlog/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MiniBlog.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
